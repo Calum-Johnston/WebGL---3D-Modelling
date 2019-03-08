7f724ff247bbc23b93931be36ec946d21be0f2c8
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-03-08 19:48:23 +0000

    Added a translated triangle with seperate vertex colours

--- triangles/test.js
+++ triangles/test.js
@@ -1,22 +1,23 @@
-// HelloTriangle.js (c) 2012 matsuda
 // Vertex shader program
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
+    'varying vec4 v_Color;\n' +
+    'uniform vec4 u_Translation;\n' +
     'void main() {\n' +
-    '  gl_Position = a_Position;\n' + //Coordinates
-    '  gl_PointSize = 10;\n' +  // Point Size
+    '  gl_Position = a_Position + u_Translation;\n' + //Coordinates
+    '  v_Color = a_Color;\n' + 
     '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
     'precision mediump float;\n' +
-    'uniform vec4 u_FragColor;\n' + 
+    'varying vec4 v_Color;\n' +
     'void main() {\n' +
-    '  gl_FragColor = u_FragColor;\n' +  // Point Color
+    '  gl_FragColor = v_Color;\n' +  // Point Color
     '}\n';
 
-
-
+var Tx = 0.5, Ty = 0.5, Tz = 0.0;
 
 function main() {
     // Retrieve <canvas> element
@@ -44,31 +45,16 @@ function main() {
 
     
     /** =========================
-        Storage Locations   
-    ========================= **/
-	  // Get the storage location of attribute variables (VERTEX SHADER)
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-	  if(a_Position < 0){
-		  console.log('Failed to get the storage location of a_Position');
+        Set positions of vertices   
+    ========================= **/	  
+    var n = initVertexBuffers(gl);
+    if(n < 0){
+      console.log('Failed to set the positions of the vertices');
+      return;
     }
-    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-    if(a_PointSize < 0){
-      console.log('Failed to get the storage location of a_PointSize');
-    }
-
-    // Get the storage location of attribute variables (FRAGMENT SHADER)
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-    if(u_FragColor < 0){
-      console.log('FAiled to get the storage location of u_FragColor');
-    }
-
-
-    /** =========================
-        Important   
-    ========================= **/
-    // Register function (event handler) to be called on a mouse press
-    canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor);};
   
+    var u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+    gl.uniform4f(u_Translation, Tx, Ty, Tz, 0.0);
 
     /** =========================
         Colour for cleaing Canvas  
@@ -87,50 +73,55 @@ function main() {
     /** =========================
         Draw   
     ========================= **/
+    gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 
-var g_points = []; // The array to store the position of a point
-var g_colors = []; // The array to store the color of a point
 
-function click(ev, gl, canvas, a_Position, u_FragColor){
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
+function initVertexBuffers(gl){
+  
+  var n = 3;
 
-  // Transform coordinates from the browser area to canvas (rect.x)
-  // then transforms into coordinates WebGL understands (canvas.x)
-  x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
-  y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+  // Defines the vertices position and colour
+  var verticesColors = new Float32Array([
+    0.0, 0.5, 0.0, 1.0, 0.0,
+    -0.5, -0.5, 1.0, 0.0, 0.0,
+    0.5, -0.5, 0.0, 0.0, 1.0,   
+  ]);
 
-  // Stores the coordinates to g_points array
-  g_points.push([x,y]);
-
-  // Stores the color to g_colors array
-  if(x >= 0.0 && y >= 0.0){
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  //Red
-  } else if(x < 0.0 && y < 0.0){
-    g_colors.push([0.0, 255.0, 0.0, 1.0]);  //Green
-  } else if(x < 0.0 && y >= 0.0){
-    g_colors.push([0.0, 0.0, 1.0, 1.0]);  //Blue
-  } else{
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  //Red
+  // Create buffer object
+  var vertexBuffer = gl.createBuffer();
+  if(!vertexBuffer){
+    console.log('Failed to create vertex buffer');
   }
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // Bind buffer object to target (tells webgl what type of data the buffer object cnntains)
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write data into the buffer object (bound by the first parameter)
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
 
-  var len = g_points.length
-  for(var i = 0; i < len; i++){
-    var xy = g_points[i];
-    var rgba = g_colors[i];
+  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
 
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINT, 0, 1);
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	if(a_Position < 0){
+	  console.log('Failed to get the storage location of a_Position');
   }
+
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  if(a_Color < 0){
+    console.log('Failed to get the storage location of a_Color');
+  }
+
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+  gl.enableVertexAttribArray(a_Color);
+
+  return n;
 }
 
 
